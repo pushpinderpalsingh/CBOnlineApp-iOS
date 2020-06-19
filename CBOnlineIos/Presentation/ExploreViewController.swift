aecc54e5cf1c4ddc230018169492d3dca5023418
Pushpinder Pal Singh <pushpinder@tutamail.com>
2020-06-19 08:51:19 +0530

    Complete Nav Bar and Tab Bar

---
+++ CBOnlineIos/Presentation/ExploreViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ExploreViewController.swift
+//  CBOnlineIos
+//
+//  Created by Pushpinder Pal Singh on 19/06/20.
+//  Copyright © 2020 Coding Blocks. All rights reserved.
+//
+
+import UIKit
+
+class ExploreViewController: UIViewController {
+
+    @IBOutlet weak var navBarView: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navBarView.layer.cornerRadius = 20
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
